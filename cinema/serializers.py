83364ccd69196f05f3352ac3b94f108c7f082ac8
Aleksey Pastukhov <mindflux99@gmail.com>
2025-02-16 21:58:00 +0200

    Implement task

--- cinema/serializers.py
+++ cinema/serializers.py
@@ -38,6 +38,7 @@ class MovieImageSerializer(serializers.ModelSerializer):
 
 class MovieSerializer(serializers.ModelSerializer):
     image = serializers.ImageField(read_only=True)
+
     class Meta:
         model = Movie
         fields = ("id",
